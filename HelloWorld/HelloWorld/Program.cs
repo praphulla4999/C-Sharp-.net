aeaa067a359e35db9862e6005fddbddb67e22e7d
praphulla4999 <p.koli.4999@gmail.com>
2017-04-27 20:51:22 +0530

    Chapter-1
    Starting simple tutorials for C#.net.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is the basic program. Does not do much
+
+            Console.WriteLine("Hello World! Welcome to C#!"); // Console.Writeline() used to display or print output on screen
+            Console.ReadKey();
+            // You can also use  ReadKey() method, so that program does not go further unless you enter some key
+        }
+    }
+}
