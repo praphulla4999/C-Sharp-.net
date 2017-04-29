fa4d8c0f05551033558db3f14018ee1d02b5d100
praphulla4999 <p.koli.4999@gmail.com>
2017-04-29 17:39:08 +0530

    Ch2
    Classes and its properties

---
+++ Simple_class/Simple_class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Declaring new class
+public class My_class
+{
+    private int data=2017;
+
+    public int show_data()
+    {
+        return data;
+    }
+
+};
+
+namespace Simple_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            My_class exmaple = new My_class();  // Creating object of My_Class
+
+            Console.WriteLine(exmaple.show_data()); // Using show_data() function using class object
+            Console.ReadKey();
+
+        }
+    }
+}
